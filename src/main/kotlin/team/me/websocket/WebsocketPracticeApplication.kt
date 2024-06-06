d504e40d41fa6d270d23b5b210f2e832b76c076c
BrianDYKim <18k7102dy@gmail.com>
2024-06-06 15:14:21 +0900

    initial commit

---
+++ src/main/kotlin/team/me/websocket/WebsocketPracticeApplication.kt
@@ -0,0 +1,11 @@
+package team.me.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/team/me/websocket/WebsocketPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.me.websocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
